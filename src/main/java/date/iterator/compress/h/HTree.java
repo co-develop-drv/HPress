7278cc07359dcbb1e61a3f020667713cc25775bb
co-develop-drv <draculav@hotmail.com>
2020-08-11 12:33:09 +0800

    note

--- src/main/java/date/iterator/compress/h/HTree.java
+++ src/main/java/date/iterator/compress/h/HTree.java
@@ -3,4 +3,14 @@ package date.iterator.compress.h;
 public class HTree {
     // 当前词
     private byte current;
+
+    public void input() {
+        // 分词
+        // 统计每个词的频率做权重
+        // build tree ,相同频率且位置总是相邻的合并
+    }
+
+    private void splitWords() {
+
+    }
 }
---
+++ src/test/java/date/iterator/compress/h/HTreeTest.java
@@ -0,0 +1,10 @@
+package date.iterator.compress.h;
+
+import org.junit.Test;
+
+public class HTreeTest {
+    @Test
+    public void input(){
+
+    }
+}
